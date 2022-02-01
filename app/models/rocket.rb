bced8f7370b079ccd1eb2b9c9180cd3629a6960a
Frazer Gavin <fcgavin27@gmail.com>
2022-02-01 20:43:14 +0000

    routes completed for rocket user and booking

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -2,5 +2,4 @@ class Rocket < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :description, :price, :capacity, :range, :power, presence: true
-  validates :name, uniqueness: true
 end
