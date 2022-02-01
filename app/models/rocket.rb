893e8faea22bf046fcb6048b4bcb84ba0aea4ea9
Frazer Gavin <fcgavin27@gmail.com>
2022-02-01 20:23:29 +0000

    completed validations for the models

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,3 +1,6 @@
 class Rocket < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :name, :description, :price, :capacity, :range, :power, presence: true
+  validates :name, uniqueness: true
 end
