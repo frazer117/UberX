15670e6ac4d58d5458beee0d619f6627c79fe080
Fang Zhao <fangzhao021@hotmail.com>
2022-02-08 17:58:24 +0000

    added image display and upload.cleaned up home page and index page to see image effect

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,5 +1,6 @@
 class Rocket < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   validates :name, :description, :price, :capacity, :range, :power, presence: true
 end
