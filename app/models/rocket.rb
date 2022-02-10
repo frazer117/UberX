31f2efdba5fba9a0ef24dfe83465b354b775232e
Frazer Gavin <fcgavin27@gmail.com>
2022-02-10 19:07:04 +0000

    geocoding for index page

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,5 +1,7 @@
 class Rocket < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :name, :description, :price, :capacity, :range, :power, presence: true
+  validates :name, :description, :price, :capacity, :range, :power, :address, :longitude, :latitude, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
