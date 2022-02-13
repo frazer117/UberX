1cacb9f404da98d34b37cabd66261e9fea913c14
Fang Zhao <fangzhao021@hotmail.com>
2022-02-13 14:09:54 +0000

    added cancel booking and more seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
 
-file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
-
 Booking.destroy_all
 Rocket.destroy_all
 User.destroy_all
@@ -18,53 +16,66 @@ user = User.create!(
   password: 'password-2021'
 )
 
-# rocket = Rocket.new(
-#   name: 'Falcon 1',
-#   user_id: user.id,
-#   description: 'Reusable two-stage rocket ideal for quick weekend getaways to the Moon and Mars. Stylish interior and sleek exterior design. Also comes with home cinema and cupholders.',
-#   price: 750000,
-#   capacity: 4,
-#   range: 400,
-#   power: 75000,
-#   address: Stockbridge, SO20 8DY, England
-#   is_available: true)
-
-# rocket.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-
-Rocket.create!(
+rocket = Rocket.new(
   name: 'Falcon 1',
   user: User.last,
   description: 'Reusable two-stage rocket ideal for quick weekend getaways to the Moon and Mars. Stylish interior and sleek exterior design. Also comes with home cinema and cupholders.',
-  price: 750000,
+  price: 7500,
   capacity: 4,
   range: 400,
   power: 75000,
   address: 'London',
-
   is_available: true
 )
 
-Rocket.create!(
+file = URI.open('https://res.cloudinary.com/dajifvnn5/image/upload/v1644409429/rocket_image_tpd90z.jpg')
+rocket.photo.attach(io: file, filename: 'rocket_image_tpd90z.jpg', content_type: 'image/jpg')
+rocket.save
+
+rocket = Rocket.new(
   name: 'Massive Thruster',
   user: User.last,
   description: 'Ideal for reaching the outter solar system for that long summer vacation.',
-  price: 1950000,
+  price: 19500,
   capacity: 6,
   range: 800,
   power: 25000,
-  address: 'Sydney',
-
+  address: 'Paris',
   is_available: true
 )
 
-# Rocket.create!(
-#   name: 'Falcon 1',
-#   user_id: user.id,
-#   description: 'Reusable two-stage rocket ideal for quick weekend getaways to the Moon and Mars. Stylish interior and sleek exterior design. Also comes with home cinema and cupholders.',
-#   price: 750000,
-#   capacity: 4,
-#   range: 400,
-#   power: 75000,
-#   address: Stockbridge, SO20 8DY, England
-#   is_available: true
-# )
+file = URI.open('https://res.cloudinary.com/dajifvnn5/image/upload/v1644417653/rocket_3_kuy1f1.jpg')
+rocket.photo.attach(io: file, filename: 'rocket_3_kuy1f1.jpg', content_type: 'image/jpg')
+rocket.save
+
+rocket = Rocket.new(
+  name: 'Apollo',
+  user: User.last,
+  description: 'A morden design and spacious roket taking you everywhere you want in galaxy.',
+  price: 10000,
+  capacity: 8,
+  range: 900,
+  power: 30000,
+  address: 'Seattle',
+  is_available: true
+)
+
+file = URI.open('https://res.cloudinary.com/dajifvnn5/image/upload/v1644417653/rocket_1_bydhuf.jpg')
+rocket.photo.attach(io: file, filename: 'rocket_1_bydhuf.jpg', content_type: 'image/jpg')
+rocket.save
+
+rocket = Rocket.new(
+  name: 'Dreamliner',
+  user: User.last,
+  description: 'Just like the name: take you to your dream planet.',
+  price: 12000,
+  capacity: 8,
+  range: 950,
+  power: 35000,
+  address: 'New York',
+  is_available: true
+)
+
+file = URI.open('https://res.cloudinary.com/dajifvnn5/image/upload/v1644417653/rocket_6_ies050.jpg')
+rocket.photo.attach(io: file, filename: 'rocket_6_ies050.jpg', content_type: 'image/jpg')
+rocket.save
